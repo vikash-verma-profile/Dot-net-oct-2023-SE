196fefd827e0baf83f970d9c5711a31127389be9
Level-Up-4 <vikash-verma-profile>
2023-10-18 12:54:48 +0530

    added

--- Labs/Day-1/Day-1/Program.cs
+++ Labs/Day-1/Day-1/Program.cs
@@ -6,27 +6,44 @@ namespace Day_1
     {
         static void Main(string[] args)
         {
-            int a = 1;
-            int b = 5;
+            int Number1 = 1;
+            int Number2 = 5;
+            int Number3 = 10;
 
-            Console.WriteLine("Please enter some value : ");
-            string inputvalue = Console.ReadLine();
-            Console.WriteLine("The value that you have entered is "+inputvalue);
-            //<data type> variable-name
-            /*
-             asdasdasdasd
-             asdasd*/
-            Console.WriteLine("Hello World!jhg asdjhas djhg asdjh asdjfa sd");
-            Console.WriteLine(a+b);
-
-            if (a > b)
+            if (Number1 > Number2 && Number1>Number3 )
             {
-                Console.WriteLine(a+" is greater than "+b);
+                Console.WriteLine(Number1+" greater Number");
+            }
+            else if (Number2> Number1 && Number2 > Number3)
+            {
+                Console.WriteLine(Number2 + " greater Number");
             }
             else
             {
-                Console.WriteLine(b+" is greater than "+a);
+                Console.WriteLine(Number3 + " is greater Number");
             }
+
+
+
+
+            //Console.WriteLine("Please enter some value : ");
+            //string inputvalue = Console.ReadLine();
+            //Console.WriteLine("The value that you have entered is "+inputvalue);
+            ////<data type> variable-name
+                ///*
+                // asdasdasdasd
+                // asdasd*/
+                //Console.WriteLine("Hello World!jhg asdjhas djhg asdjh asdjfa sd");
+                //Console.WriteLine(a+b);
+
+                //if (a > b)
+                //{
+                //    Console.WriteLine(a+" is greater than "+b);
+                //}
+                //else
+                //{
+                //    Console.WriteLine(b+" is greater than "+a);
+                //}
         }
     }
 }
---
+++ Labs/Day-1/Day-1/new-program/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_1.new_program
+{
+    class Class1
+    {
+        static void Main1(string[] args)
+        {
+            int a = 1;
+            int b = 5;
+
+            Console.WriteLine("Please enter some value : ");
+            string inputvalue = Console.ReadLine();
+            Console.WriteLine("The value that you have entered is " + inputvalue);
+            //<data type> variable-name
+            /*
+             asdasdasdasd
+             asdasd*/
+            Console.WriteLine("Hello World!jhg asdjhas djhg asdjh asdjfa sd");
+            Console.WriteLine(a + b);
+
+            if (a > b)
+            {
+                Console.WriteLine(a + " is greater than " + b);
+            }
+            else
+            {
+                Console.WriteLine(b + " is greater than " + a);
+            }
+        }
+    }
+}
