b72489e9ae35c038e03127509dc5a4ca419b1fc9
Level-Up-4 <vikash-verma-profile>
2023-10-18 12:29:39 +0530

    added first program

--- Labs/Day-1/Day-1/Program.cs
+++ Labs/Day-1/Day-1/Program.cs
@@ -6,7 +6,27 @@ namespace Day_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 5;
+
+            Console.WriteLine("Please enter some value : ");
+            string inputvalue = Console.ReadLine();
+            Console.WriteLine("The value that you have entered is "+inputvalue);
+            //<data type> variable-name
+            /*
+             asdasdasdasd
+             asdasd*/
+            Console.WriteLine("Hello World!jhg asdjhas djhg asdjh asdjfa sd");
+            Console.WriteLine(a+b);
+
+            if (a > b)
+            {
+                Console.WriteLine(a+" is greater than "+b);
+            }
+            else
+            {
+                Console.WriteLine(b+" is greater than "+a);
+            }
         }
     }
 }
