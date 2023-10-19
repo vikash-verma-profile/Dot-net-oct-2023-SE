bc29e509f3b6798dcdcb0a14f79a6be7fce96cc8
Level-Up-4 <vikash-verma-profile>
2023-10-19 09:13:54 +0530

    added code

---
+++ Labs/Day-2/Day-2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int inputValue = 12;
+            switch (inputValue)
+            {
+                case 0:
+                    Console.WriteLine("Hi i am case 0");
+                    break;
+                case 2:
+                    Console.WriteLine("Hi i am case 2");
+                    break;
+                case 4:
+                    Console.WriteLine("Hi i am case 3");
+                    break;
+                default:
+                    Console.WriteLine("This output is coming from default block");
+                    break;
+            }
+        }
+    }
+}
