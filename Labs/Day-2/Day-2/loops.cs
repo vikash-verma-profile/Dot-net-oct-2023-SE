e8b2db64d2d93273e8b0befe15138db65dd8fbdf
Level-Up-4 <vikash-verma-profile>
2023-10-19 09:40:31 +0530

    added code

---
+++ Labs/Day-2/Day-2/loops.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    internal class loops
+    {
+        public static void Main()
+        {
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine("Hi I a from for loop");
+            }
+            int counter = 0;
+            while (counter > 0)
+            {
+                Console.WriteLine("Hi I a from while loop");
+                counter++;
+            }
+
+            do
+            {
+                Console.WriteLine("Hi I a from do while loop");
+                //counter++;
+            } while (counter>0);
+        }
+    }
+}
