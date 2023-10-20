f114cfc02a944983ce3261fcb721ce4683ea5541
Level-Up-4 <vikash-verma-profile>
2023-10-20 12:44:03 +0530

    add

--- Labs/Day-3/Day-3/class5.cs
+++ Labs/Day-3/Day-3/class5.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Day_3
 {
+    //encapsulation and abstraction
     class Student
     {
         //properties
@@ -15,18 +16,45 @@ namespace Day_3
         //destructors
         //indexers
         //methods
-       public int rollNo; //camel
-       public string studentName;
+        //public int rollNo; //camel
+        //public string studentName;
+
+        int rollNo;
+        string studentName;
+
+        public void GetStudentData()
+        {
+            Console.WriteLine("Please enter student name");
+            studentName = Console.ReadLine();
+            Console.WriteLine("Please enter roll No");
+            rollNo=Convert.ToInt32(Console.ReadLine());
+        }
+        public void PrintStudentData()
+        {
+            Console.WriteLine("Student RollNo :" + rollNo);
+            Console.WriteLine("Student Name :"+studentName);
+        }
     }
     internal class class5
     {
         public static void Main()
         {
-            Student std=new Student();
-            std.rollNo = 1;
-            std.studentName = "Vikash";
-            Console.WriteLine(std.rollNo);
-            Console.WriteLine(std.studentName);
+            Student[] StudentList = new Student[2]; 
+            for (int i=0; i<StudentList.Length; i++) {
+                Student std = new Student();
+                std.GetStudentData();
+                StudentList[i]= std;
+            }
+            for (int i = 0; i < StudentList.Length; i++)
+            {
+                StudentList[i].PrintStudentData();
+            }
+            //std.rollNo = 1;
+            //std.studentName = "Vikash";
+            //Console.WriteLine(std.rollNo);
+            //Console.WriteLine(std.studentName);
+            //std.GetStudentData();
+            //std.PrintStudentData();
         }
     }
 }
