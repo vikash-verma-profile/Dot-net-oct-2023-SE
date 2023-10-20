248411cec6cc70fc85a213490da94de498f487b2
Level-Up-4 <vikash-verma-profile>
2023-10-20 12:27:20 +0530

    add

---
+++ Labs/Day-3/Day-3/class5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3
+{
+    class Student
+    {
+        //properties
+        //fields
+        //instance variables
+        //constructors
+        //destructors
+        //indexers
+        //methods
+       public int rollNo; //camel
+       public string studentName;
+    }
+    internal class class5
+    {
+        public static void Main()
+        {
+            Student std=new Student();
+            std.rollNo = 1;
+            std.studentName = "Vikash";
+            Console.WriteLine(std.rollNo);
+            Console.WriteLine(std.studentName);
+        }
+    }
+}
--- Labs/Day-3/Day-3/conversion4.cs
+++ Labs/Day-3/Day-3/conversion4.cs
@@ -8,7 +8,7 @@ namespace Day_3
 {
     internal class conversion4
     {
-        public static void Main()
+        public static void Main4()
         {
             //int a = Convert.ToInt32("123");
             //Int64 b = Convert.ToInt64("878");
