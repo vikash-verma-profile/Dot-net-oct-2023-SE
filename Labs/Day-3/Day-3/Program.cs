b372dbf6523bae676ab03bace53a22b4cf06ca4e
Level-Up-4 <vikash-verma-profile>
2023-10-20 09:27:16 +0530

    add

---
+++ Labs/Day-3/Day-3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 2];
+            Console.WriteLine("Please enter values");
+            for (int row = 0; row < 2; row++)
+            {
+
+                for (int col = 0; col < 2; col++)
+                {
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Your matrix is");
+            for (int row = 0; row < 2; row++)
+            {
+
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write(matrix[row, col]+"\t");
+                }
+                Console.WriteLine();
+            }
+            //Console.WriteLine("Hello, World!");
+
+            //Console.Write("Hello"+"\n");
+            //Console.Write("Hello" + "\n");
+        }
+    }
+}
