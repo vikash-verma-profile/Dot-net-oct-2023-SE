9fbc452edf013cc363ecf55782366274d198c2d2
Level-Up-4 <vikash-verma-profile>
2023-10-20 11:30:37 +0530

    add

--- Labs/Day-3/Day-3/conversion4.cs
+++ Labs/Day-3/Day-3/conversion4.cs
@@ -8,13 +8,18 @@ namespace Day_3
 {
     internal class conversion4
     {
-        public static void Main()
+        public static void Main4()
         {
-            int a = Convert.ToInt32("123");
-            Int64 b = Convert.ToInt64("878");
-            string s = 89.ToString();
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+            //int a = Convert.ToInt32("123");
+            //Int64 b = Convert.ToInt64("878");
+            ////=Convert.ToBoolean
+            ////    Convert.ToDecimal
+            ////    Convert.
+            //string s = 89.ToString();
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            int output;
+            //int a = int.TryParse("123");
         }
     }
 }
---
+++ Labs/Day-3/Day-3/functions5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3
+{
+    //in vs out vs ref
+    // ref vs out
+    internal class functions5
+    {
+        //out paramerter should be assigned some value inside function before 
+        //returning
+        public static void sumOut(in int a,in int b,out int c)
+        {
+             c = a + b;
+            //Console.WriteLine(a + b);
+        }
+        //ref should be assigned before passing to a function
+        public static void sumRef(ref int a,ref int b)
+        {
+            Console.WriteLine(a+b);
+        }
+       
+        public static void Main()
+        {
+            int result,Number1, Number2;
+            Number1 = 1;
+            Number2=2;
+            sumOut(Number1, Number2, out result);
+            sumRef(ref Number1,ref Number2);
+            Console.WriteLine(result);
+        }
+    }
+}
