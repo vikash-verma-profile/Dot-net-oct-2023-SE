c012f849e7bcebda3485192bd08368789f23bec6
Level-Up-4 <vikash-verma-profile>
2023-10-30 09:19:54 +0530

    added boxing

---
+++ Labs/Day-6/Day-6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            object b;
+            b = a;//boxing
+            Console.WriteLine(b);
+            b = 2;
+            a = (int)b; //un-boxing
+            Console.WriteLine(a);
+        }
+    }
+}
