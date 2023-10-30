40bdc7407901bf1e52af606f8f671a4c82bf9646
Level-Up-4 <vikash-verma-profile>
2023-10-30 09:58:56 +0530

    added boxing

--- Labs/Day-6/Day-6/isvsas2.cs
+++ Labs/Day-6/Day-6/isvsas2.cs
@@ -12,18 +12,57 @@ namespace Day_6
     }
     public class P1 : P { }
     public class P2 { }
+
+    class x
+    {
+
+    }
+    class y
+    {
+
+    }
     internal class isvsas2
     {
         public static void Main()
         {
+            /* is operator
             P o1 = new P();
             P1 o2=new P1();
-            Console.WriteLine(o1 is P);
+            
+            var result = o1 is P;
+            Console.WriteLine(result);
             Console.WriteLine(o1 is Object);
             Console.WriteLine(o2 is P1);
             Console.WriteLine(o2 is Object);
             Console.WriteLine(o1 is P2);
             Console.WriteLine(o2 is P2);
+
+            */
+
+            /* as Operator*/
+
+            object[] o = new object[5];
+            o[0] = new y();
+            o[1] = new x();
+            o[2] = "Vikash";
+            o[3] = 232323.0;
+            o[4] = null;
+
+            foreach (var x in o)
+            {
+                string str = x as string;
+               
+                if(str!=null)
+                {
+                    Console.WriteLine(str);
+                }
+                else
+                {
+                    Console.WriteLine("it is not a string value");
+                }
+            }
+
+
         }
     }
 }
