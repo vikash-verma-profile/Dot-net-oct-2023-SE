c0b49111158310777ab7fea8b2fbbaaf36fd1759
Level-Up-4 <vikash-verma-profile>
2023-10-31 09:23:44 +0530

    added code

---
+++ Labs/Day-7/Day-7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Number1, Number2;
+                Number1 = 4;
+                Number2 = 0;
+                decimal result = Number1 / Number2;
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Some error have occured");
+            }
+            
+        }
+    }
+}
