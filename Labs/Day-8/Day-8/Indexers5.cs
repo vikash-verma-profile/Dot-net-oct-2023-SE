3101bc9f4b863dc53b72d807598c90c3c7ec0657
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-11-02 12:55:07 +0530

    Extension method

--- Labs/Day-8/Day-8/Indexers5.cs
+++ Labs/Day-8/Day-8/Indexers5.cs
@@ -20,12 +20,32 @@ namespace Day_8
         //automatic properties
         public string Name { get; set; }
     }
+    class IndexerCreation
+    {
+        private string[] val = new string[4];
+        public string this[int index]
+        { get { return val[index]; }
+          set { val[index] = value; }
+        }
+    }
     internal class Indexers5
     {
         public static void Main()
         {
             Dummy d=new Dummy();
             d.id = 1;
+            Console.WriteLine(d.id);
+
+            IndexerCreation c=new IndexerCreation();
+            c[0] = "C";
+            c[1] = "C++";
+            c[2] = "C#";
+            c[3] = "java";
+
+            Console.WriteLine(c[0]);
+            Console.WriteLine(c[1]);
+            Console.WriteLine(c[2]);
+            Console.WriteLine(c[3]);
         }
     }
 }
