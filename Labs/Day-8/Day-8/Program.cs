b01e3ae0d8562b6d48ec3a139186d719f8b33aad
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-11-02 09:27:18 +0530

    added regex code

---
+++ Labs/Day-8/Day-8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "8882057008";
+            string PatternToBematched = "^[0-9]{10}$";
+            Regex rx = new Regex(PatternToBematched);
+            if (rx.IsMatch(str))
+            {
+                Console.WriteLine("Your number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Your number is Invalid");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
