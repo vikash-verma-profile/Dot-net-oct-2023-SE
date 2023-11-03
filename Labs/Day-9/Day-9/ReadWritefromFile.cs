fcef39fbbd7406629246c2bcedfb86b72598caa5
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-11-03 12:24:50 +0530

    added collections

--- Labs/Day-9/Day-9/ReadWritefromFile.cs
+++ Labs/Day-9/Day-9/ReadWritefromFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Day_9
 {
@@ -18,20 +19,25 @@ namespace Day_9
     }
     internal class ReadWritefromFile
     {
-        public static void WriteInFile(string filePath,Employee employee)
+        public static void WriteInFile(string filePath,List<Employee> employee)
         {
-            File.WriteAllText(filePath, employee.ToString());
+            var jsonData=JsonConvert.SerializeObject(employee);
+            File.WriteAllText(filePath, jsonData);
         }
-        public static string ReadFromFile(string filePath)
+        public static List<Employee> ReadFromFile(string filePath)
         {
-            return File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Employee>>(File.ReadAllText(filePath));
         }
         public static void Main()
         {
             string filePath = "C:\\Users\\LEVEL UP SOLUTIONS 1\\Desktop\\Dot-net-oct-2023-SE\\Labs\\Day-9\\Day-9\\file\\Textfile.txt";
-            WriteInFile(filePath,new Employee() { EmployeeID="Emp101",EmployeeName="Vikash"});
+            WriteInFile(filePath,new List<Employee> { new Employee() { EmployeeID = "Emp101", EmployeeName = "Vikash" } });
             Console.WriteLine("Your content is being written");
-            Console.WriteLine(ReadFromFile(filePath));
+            foreach (var item in ReadFromFile(filePath))
+            {
+                Console.WriteLine(item.EmployeeID+" | "+item.EmployeeName);
+            }
+           
         }
     }
 }
