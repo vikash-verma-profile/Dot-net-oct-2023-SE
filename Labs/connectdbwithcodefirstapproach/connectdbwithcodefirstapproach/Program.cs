0e18e897451e11a2dbbd544b55b83f1ea880b9db
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-11-17 12:13:53 +0530

    added code

--- Labs/connectdbwithcodefirstapproach/connectdbwithcodefirstapproach/Program.cs
+++ Labs/connectdbwithcodefirstapproach/connectdbwithcodefirstapproach/Program.cs
@@ -19,6 +19,26 @@ namespace connectdbwithcodefirstapproach
             {
                 Console.WriteLine(item.ID + " | " + item.Name);
             }
+            Console.WriteLine("==============Order By============================");
+            var employeeListOrder=db.Employees.OrderBy(x => x.ID).ToList();
+            var employeeListOrderDesc = db.Employees.OrderByDescending(x => x.ID).ToList();
+            foreach (var item in employeeListOrder)
+            {
+                Console.WriteLine(item.ID + " | " + item.Name);
+            }
+            foreach (var item in employeeListOrderDesc)
+            {
+                Console.WriteLine(item.ID + " | " + item.Name);
+            }
+            Console.WriteLine("==============Group By============================");
+            var employeeListGroup = db.Employees.GroupBy(x => x.Name).Select(x => new {Name=x.FirstOrDefault().Name}).ToList();
+            foreach (var item in employeeListGroup)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("==============First Vs FirstORDefault============================");
+            var employeeListFirst = db.Employees.Where(x => x.ID == 10).FirstOrDefault();
+         
         }
     }
 }
